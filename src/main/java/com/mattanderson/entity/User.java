4139d5ef6902aacd7ce79674340af9882140c001
Matt Anderson <matt.anderson0523@gmail.com>
2020-02-11 19:27:53 -0600

    Adds Hibernate annotation to User class

--- src/main/java/com/mattanderson/entity/User.java
+++ src/main/java/com/mattanderson/entity/User.java
@@ -1,12 +1,24 @@
 package com.mattanderson.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+
+@Entity(name = "User")
+@Table(name = "USERS")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private int id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column (name = "last_name")
     private String lastName;
+    @Column(name = "user_name")
     private String userName;
-    private String userRole; //Should I create user role if there isn't any distinction at this point? Should it be an object?
+    @Column(name = "user_role_id")
+    private int userRoleId; //Should I create user role if there isn't any distinction at this point? Should it be an object?
 
 
 
