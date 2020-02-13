d602d7e1c24ce42f6c77c3766afcc3fa640fca0c
Matt Anderson <matt.anderson0523@gmail.com>
2020-02-12 20:15:54 -0600

    Updates User class with constructors, getters, setters, and javadoc

--- src/main/java/com/mattanderson/carbConscious/entity/User.java
+++ src/main/java/com/mattanderson/carbConscious/entity/User.java
@@ -3,6 +3,11 @@ package com.mattanderson.carbConscious.entity;
 import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 
+/**
+ * Defines a User object to represent a registered user.
+ * @author Matt Anderson
+ * @version 11
+ */
 @Entity(name = "User")
 @Table(name = "USERS")
 public class User {
@@ -20,6 +25,122 @@ public class User {
     //@Column(name = "user_role_id")
     //private List<UserRole> userRoles; //Touched base with Paula -- likely need this to be a list of objects -- comment out for Exercise 4
 
+    /**
+     * Instantiates a new User.
+     */
+    public User() {
 
+    }
 
+    /**
+     * Instantiates a new User.
+     *
+     * @param firstName the first name
+     * @param lastName  the last name
+     * @param userName  the user name
+     */
+    public User(String firstName, String lastName, String userName) {
+        this();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param firstName the first name
+     * @param lastName  the last name
+     * @param userName  the user name
+     * @param id        the id
+     */
+    public User(String firstName, String lastName, String userName, int id) {
+        this();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.id = id;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets user name.
+     *
+     * @param userName the user name
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
 }
