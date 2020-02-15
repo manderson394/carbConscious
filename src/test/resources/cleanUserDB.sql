842ecd5f76fe3cd02d94d501952a8db2d6a41f3c
Matt Anderson <matt.anderson0523@gmail.com>
2020-02-15 11:48:37 -0600

    Adds user cleaning SQL file

---
+++ src/test/resources/cleanUserDB.sql
@@ -0,0 +1,6 @@
+DELETE FROM USERS;
+INSERT INTO USERS VALUES ('Matt', 'Anderson', 'mattanderson', 'matt@gmail.com', 'testing', '1/1/2020', '1/2/2020');
+INSERT INTO USERS VALUES ('Drew', 'Peterson', 'drewPeterson', 'drew@madisoncollege.edu', 'testing1', '1/4/2020', '1/5/2020');
+INSERT INTO USERS VALUES ('Nicole', 'Sannes', 'nicoleSannes', 'nicole@aol.com', 'testing2', '1/7/2020', '1/8/2020');
+INSERT INTO USERS VALUES ('Shelley', 'Anderson', 'shelleyAnderson1', 'shelley@live.com', 'testing3', '1/7/2020', '1/8/2020');
+INSERT INTO USERS VALUES ('Rich', 'Anderson', 'richAnderson2', 'rich@gmail.com', 'testing3', '1/10/2020', '1/23/2020');
